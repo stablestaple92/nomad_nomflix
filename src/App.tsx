fc3c1d119dd476ae17d8511efae44afa453863f7
Lim Chang-Hyun <stablestaple92@gmail.com>
2022-04-26 11:13:05 +0900

    header update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
-
+/**
+ * react-router-dom
+ * 
+ * path="/" 는 맨 마지막에 배치해야한다!
+ * 
+ */
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="tv" element={<Tv/>} />
+        <Route path="search" element={<Search/>} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </Router>
+
   );
 }
 
