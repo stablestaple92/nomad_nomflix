8779adde41f79928e4d81eae76f4f49982e634eb
Lim Chang-Hyun <stablestaple92@gmail.com>
2022-04-26 18:45:51 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,9 @@ function App() {
       <Routes>
         <Route path="tv" element={<Tv/>} />
         <Route path="search" element={<Search/>} />
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home/>}>
+          <Route path="/movies/:movieId" />
+        </Route>
       </Routes>
     </Router>
 
--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,205 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Navigate, useMatch, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { getMovies, IGetMoviesResult } from "../api";
+import { makeImagePath } from "./utils";
 
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{bgPhoto: string}>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)) , url(${props => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{bgPhoto: string}>`
+  background-color: white;
+  background-image: url(${props => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  color: ${props => props.theme.white.lighter};
+  font-size: 66px;
+  cursor: pointer;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`; 
+
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${props => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 5,
+  },
+  visible: {
+    x: 0
+  },
+  exit: {
+    x: -window.outerWidth - 5,
+  }
+}
+
+const BoxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: {
+      delay: 0.5,
+      duration: 0.3,
+      type: "tween",
+    }
+  },
+}
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.3,
+      type: "tween",
+    }
+  },
+}
+
+const offset = 6;
+
+/**
+ * 
+ * onExitComplete
+ * exit 중인 모든 노드들이 애니메이션을 끝내면 실행됩니다.
+ * 
+ * initial
+ * initial={false}를 전달하면 AnimatePresence는 컴포넌트가 처음 렌더링될 때 자식의 초기 애니메이션을 비활성화합니다.
+ * 
+ * slice()
+ * slice() 메서드는 어떤 배열의 begin부터 end까지(end 미포함)에 대한 얕은 복사본을 새로운 배열 객체로 반환합니다. 원본 배열은 바뀌지 않습니다.
+ * 
+ */
 
 function Home () {
-  return null;
+  const navigate = useNavigate();
+  const bigMovieMatch = useMatch("/movies/:movieId");
+  const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+   if (data) {
+      // transition 중복 방지
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor (totalMovies / offset) - 1;
+      setIndex(prev => (prev === maxIndex ? 0 : prev + 1))
+   }
+  };
+  const toggleLeaving = () => setLeaving(prev => !prev);
+  const onBoxClicked = (movieId:number) => {
+    navigate(`/movies/${movieId}`);
+  };
+  return (
+    <Wrapper>{isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row 
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1}}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id} 
+                      variants={BoxVariants}
+                      initial="normal"
+                      whileHover="hover"
+                      onClick={() => onBoxClicked(movie.id)}
+                      transition={{ type: "tween" }}
+                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                    >
+                      
+                      <Info
+                        variants={infoVariants}
+                      >
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
+      )}
+      </Wrapper>
+  );
 }
 
 export default Home;
